c16f232a608b8b978d39d9f7d9bcb62719f4802b
kmmakinaci <kmmakinaci@gmail.com>
2023-12-25 18:51:27 +0100

    Refactoring to cpp
    Let's write an modern c++ code, and make structure more object oriented
    mindset.

--- main.cc
+++ main.cc
@@ -1,57 +1,57 @@
 #include "calculator.h"
 #include <iostream>
 
-using namespace std;
-
 int main() 
 {
-    char operation;
-    Calculator c; 
-    cout << "Enter + to Add 2 Numbers" << 
+    std::unique_ptr<Operation> addition = std::make_unique<Addition>();
+    std::unique_ptr<Operation> substraction = std::make_unique<Subtraction>();
+    std::unique_ptr<Operation> multiplication = std::make_unique<Multiplication>();
+    std::unique_ptr<Operation> division = std::make_unique<Division>();
+
+    double number1{}, number2{};
+    char operation{};
+    double result{};
+
+    std::cout << "Enter + to Add 2 Numbers" << 
             "\nEnter - to Subtract 2 Numbers" << 
             "\nEnter * to Multiply 2 Numbers" << 
             "\nEnter / to Divide 2 Numbers" << 
             "\nEnter 0 To Exit\n";
-         
-    do
-    {
-        cout << "\nEnter Choice: ";
-        cin >> operation;
-        switch (operation)
-        {
-        case '+':
-             
-            // result function invoked
-            c.result();     
-             
-            // function to calculate summation
-            cout << "Result: " << 
-                     c.add() << endl; 
-            break;
-        case '-':
-             
-            // function to calculate subtraction
-            c.result();
-            cout << "Result: " << 
-                     c.sub() << endl; 
-            break;
-        case '*':
-            c.result();
-             
-            // function to calculate multiplication
-            cout << "Result: " << 
-                     c.mul() << endl; 
-            break;
-        case '/':
-            c.result();
-             
-            // function to calculate division
-            cout << "Result: " << 
-                     c.div() << endl; 
-            break;
+
+    do {
+        std::cout << "\nEnter Choice: ";
+        std::cin >> operation;
+
+        if(operation =='0')
+            return 0;
+
+        std::cout << "Enter First Number: ";
+        std::cin >> number1;
+
+        std::cout << "Enter Second Number: ";
+        std::cin >> number2;
+
+
+        switch(operation) {
+            case '+':
+                result = addition->perform(number1, number2);
+                break;
+            case '-':
+                result = substraction->perform(number1, number2);
+                break;
+            case '*':
+                result = multiplication->perform(number1, number2);
+                break;
+            case '/':
+                result = division->perform(number1,number2);
+                break;
+            default:
+                std::cout << "Invalid operator!" << std::endl;
+                return 1; //Exiting with an error code
         }
-         
+
+        std::cout << "Result:" << result << std::endl;
     } while (operation == '+' || operation == '-' || operation == '*' || operation == '/');
-     
+
     return 0;
 }		
