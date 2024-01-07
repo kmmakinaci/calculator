76f0d5fc596c4b8e82ee93445d0b4f979b46adff
kmmakinaci <kmmakinaci@gmail.com>
2024-01-07 16:51:28 +0100

    Refactor calculator class regarding OCP

--- calculator.hpp
+++ calculator.hpp
@@ -1,19 +1,22 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 #include <iostream>
 #include <memory>
+#include <map>
 #include <functional>
 #include "operation.hpp"
 
 // Calculator class responsible for performing calculations
 class Calculator {
 private:
-    std::function<std::unique_ptr<Operation>()> createOperation;
+    std::map<char, std::function<std::unique_ptr<Operation>()>> operationCreators;
 
 public:
-    explicit Calculator(std::function<std::unique_ptr<Operation>()> operationCreator)
-        : createOperation(std::move(operationCreator)) {}
+    explicit Calculator(std::map<char, std::function<std::unique_ptr<Operation>()>> opCreators);
 
-    double calculate(char operation, double a, double b) const {
-        std::unique_ptr<Operation> op = createOperation();
-        return op->perform(a, b);
-    }
+    void getInput(double& a, double& b, char& operation) const;
+    std::unique_ptr<Operation> createOperation(char operation) const;
+    void performCalculation() const;
 };
+#endif //CALCULATOR_H
