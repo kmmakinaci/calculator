91b0108c2331e66885f42647ac6a4464f8efc5ab
onurkaratas <onurkaratasss41@gmail.com>
2024-01-11 15:06:21 +0300

    Create operation selector file
    Select the appropriate action based on the operator entered by the user.

---
+++ operationselector.hpp
@@ -0,0 +1,27 @@
+#ifndef OPERATIONSELECTOR_H
+#define OPERATIONSELECTOR_H
+
+#include <functional>
+#include <memory>
+#include "operation.hpp"
+
+class OperationSelector {
+public:
+    static std::function<std::unique_ptr<Operation>()> getOperationCreator(char operation) {
+        switch (operation) {
+            case '+':
+                return []() { return std::make_unique<Addition>(); };
+            case '-':
+                return []() { return std::make_unique<Subtraction>(); };
+            case '*':
+                return []() { return std::make_unique<Multiplication>(); };
+            case '/':
+                return []() { return std::make_unique<Division>(); };
+            default:
+                std::cout << "Invalid operator!" << std::endl;
+                return nullptr;
+        }
+    }
+};
+
+#endif  // OPERATIONSELECTOR_H
